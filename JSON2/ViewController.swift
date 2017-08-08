87dfad5dfb0c253017c1bed6e91948873a439581
Eric J Knapp <eknapp@madisoncollege.edu>
2017-08-08 11:19:47 -0500

    Initial Commit

---
+++ JSON2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON2
+//
+//  Created by Eric J Knapp on 8/8/17.
+//  Copyright © 2017 Eric J Knapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
